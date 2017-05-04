6d987507ca6ea86b54bcc766c0885d843a0fb651
aliceminotto <i.m.e@hotmail.it>
2017-05-04 16:06:16 +0100

    issue #1 chose option number 1, updated wrapper accordingly, still in debugging'

--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -16,7 +16,7 @@ rmthis=`ls`
 echo ${rmthis}
 
 ARGSU=" ${bowtie_index} ${pair1} ${pair2} ${quality_file} ${GTF} ${read-mismatches} ${read-gap-length} ${read-edit-dist} ${read-realign-edit-dist} ${min-anchor} ${splice-mismatches} ${min-intron-length} ${max-intron-length} ${max-multihits} ${suppress-hits} ${transcriptome-max-hits} ${prefilter-multihits} ${max-insertion-length} ${max-deletion-length} ${quality_type} ${integer-quals} ${color} ${library-type} ${transcriptome-only} ${mate-inner-dist} ${mate-std-dev} ${no-novel-juncs} ${no-novel-indels} ${no-gtf-juncs} ${no-coverage-search} ${coverage-search} ${microexon-search} ${report-secondary-alignments} ${no-discordant} ${no-mixed} ${segment-mismatches} ${segment-length} ${min-coverage-intron} ${max-coverage-intron} ${min-segment-intron} ${max-segment-intron} ${keep-fasta-order} ${bowtie2-preset} ${bowtie2-N} ${bowtie2-L} ${bowtie2-i} ${bowtie2-n-ceil} ${bowtie2-gbar} ${bowtie2-mp} ${bowtie2-np} ${bowtie2-rdg} ${bowtie2-rfg} ${bowtie2-score-min} ${bowtie2-D} ${bowtie2-R} ${fusion-search} ${fusion-anchor-length} ${fusion-min-dist} ${fusion-read-mismatches} ${fusion-multireads} ${fusion-multipairs} ${fusion-ignore-chromosomes} ${rg-id} ${rg-sample} ${rg-library} ${rg-description} ${rg-platform-unit} ${rg-center} ${rg-date} ${rg-platform}"
-BOWTIEINDEXU="${bowtie_index}"
+BOWTIEINDEXU=`echo ${bowtie_index} | sed -e 's/ /, /g'`
 PAIR1CMD=`echo ${pair1} | sed -e 's/ /,/g'`
 PAIR1U=`echo ${pair1} | sed -e 's/ /, /g'`
 PAIR2CMD=`echo ${pair2} | sed -e 's/ /,/g'`
@@ -35,6 +35,31 @@ echo "Quality file(s) for read(s) 1, if provided, are ""${QUALITY_FILE1U}"
 echo "Quality file(s) for read(s) 2, if provided, are ""${QUALITY_FILE2U}"
 echo "input files are ""${INPUTSU}"
 echo "arguments are ""${ARGSU}"
+
+CMDLINEARGS=""
+#check if Bowtie index is given as an archive ar as a list of files
+#in the first case, unzip it. Otherwise check all files are here.
+#not checking if all files are in archive as i don't want to run the job in the condor-master
+if file --mime-type "./${BOWTIEINDEXU}" | grep -q gzip$
+  then
+    echo Bowtie index provided as archive
+    CMDLINEARGS+="unzip ${bowtie_index}; "
+    #get base name of index for command
+    BASEINDEX=$(basename "${bowtie_index}")
+    BASEINDEX="${BASEINDEX%%.*}"
+  else
+    if [ "${bowtie_index#*\.1\.bt2}" != "$string" ] && [ "${bowtie_index#*\.2\.bt2}" != "$string" ] && [ "${bowtie_index#*\.3\.bt2}" != "$string" ] && [ "${bowtie_index#*\.4\.bt2}" != "$string" ] && [ "${bowtie_index#*\.rev\.1\\.bt2}" != "$string" ] && [ "${bowtie_index#*\.rev\.2\.bt2}" != "$string" ]
+      then
+        echo #all files are provided. do nothing
+        temp=($bowtie_index)
+        BASEINDEX=$(basename "${temp}")
+        BASEINDEX="${BASEINDEX%%.*}"
+      else
+        >&2 echo "You are missing some index file(s), please check your input."
+        debug
+        exit 1;
+    fi
+fi
 
 CMDLINEARGS="tophat2 ${read-mismatches} ${read-gap-length} ${read-edit-dist} ${read-realign-edit-dist} ${min-anchor} ${splice-mismatches} ${min-intron-length} ${max-intron-length} ${max-multihits} ${suppress-hits} ${max-insertion-length} ${max-deletion-length} ${quality_type} ${integer-quals} ${color} ${library-type} ${mate-inner-dist} ${mate-std-dev} ${no-novel-juncs} ${no-novel-indels} ${no-gtf-juncs} ${no-coverage-search} ${coverage-search} ${microexon-search} ${report-secondary-alignments} ${no-discordant} ${no-mixed} ${segment-mismatches} ${segment-length} ${min-coverage-intron} ${max-coverage-intron} ${min-segment-intron} ${max-segment-intron} ${keep-fasta-order} ${bowtie2-n-ceil} ${bowtie2-gbar} ${bowtie2-mp} ${bowtie2-np} ${bowtie2-rdg} ${bowtie2-rfg} ${bowtie2-score-min} ${fusion-search} ${fusion-anchor-length} ${fusion-min-dist} ${fusion-read-mismatches} ${fusion-multireads} ${fusion-multipairs} ${fusion-ignore-chromosomes} ${rg-id} ${rg-sample} ${rg-library} ${rg-description} ${rg-platform-unit} ${rg-center} ${rg-date} ${rg-platform} "
 
@@ -78,9 +103,12 @@ if [ -n "${GTF}" ]
     fi
 fi
 
-CMDLINEARGS+="--num-threads 16 --output-dir output ${BOWTIEINDEXU} ${PAIR2CMD} ${PAIR2CMD} "
+CMDLINEARGS+="--num-threads 16 --output-dir output ${BASEINDEX} ${PAIR2CMD} ${PAIR2CMD} "
 
 echo ${CMDLINEARGS}
+
+exit 0 #debugging
+
 chmod +x launch.sh
 
 echo  universe                = docker >> lib/condorSubmitEdit.htc
