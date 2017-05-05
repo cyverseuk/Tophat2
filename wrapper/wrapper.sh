cbd457a033f03ff99ec5198791877341130ac0d6
aliceminotto <i.m.e@hotmail.it>
2017-05-05 12:24:06 +0100

    addressed #1 corrected shell syntax and deleted some useless conditionals

--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -48,41 +48,68 @@ if file --mime-type "./${BOWTIEINDEXU}" | grep -q gzip$
     BASEINDEX=$(basename "${bowtie_index}")
     BASEINDEX="${BASEINDEX%%.*}"
   else
-    if [ "${bowtie_index#*\.1\.bt2}" != "$string" ] && [ "${bowtie_index#*\.2\.bt2}" != "$string" ] && [ "${bowtie_index#*\.3\.bt2}" != "$string" ] && [ "${bowtie_index#*\.4\.bt2}" != "$string" ] && [ "${bowtie_index#*\.rev\.1\\.bt2}" != "$string" ] && [ "${bowtie_index#*\.rev\.2\.bt2}" != "$string" ]
+    if [ "${BOWTIEINDEXU#*\.1\.bt2}" != "${BOWTIEINDEXU}" ] && [ "${BOWTIEINDEXU#*\.2\.bt2}" != "${BOWTIEINDEXU}" ] && [ "${BOWTIEINDEXU#*\.3\.bt2}" != "${BOWTIEINDEXU}" ] && [ "${BOWTIEINDEXU#*\.4\.bt2}" != "${BOWTIEINDEXU}" ] && [ "${BOWTIEINDEXU#*\.rev\.1\.bt2}" != "${BOWTIEINDEXU}" ] && [ "${BOWTIEINDEXU#*\.rev\.2\.bt2}" != "${BOWTIEINDEXU}" ]
       then
         echo #all files are provided. do nothing
-        temp=($bowtie_index)
+        temp=(${bowtie_index})
         BASEINDEX=$(basename "${temp}")
         BASEINDEX="${BASEINDEX%%.*}"
       else
+        if [ "${BOWTIEINDEXU#*\.1\.bt2}" == "${BOWTIEINDEXU}" ]
+          then
+            >&2 echo 1.bt2 is missing
+        fi
+        if [ "${BOWTIEINDEXU#*\.2\.bt2}" == "${BOWTIEINDEXU}" ]
+          then
+            >&2 echo 2.bt2 is missing
+        fi
+        if [ "${BOWTIEINDEXU#*\.3\.bt2}" == "${BOWTIEINDEXU}" ]
+          then
+            >&2 echo 3.bt2 is missing
+        fi
+        if [ "${BOWTIEINDEXU#*\.4\.bt2}" == "${BOWTIEINDEXU}" ]
+          then
+            >&2 echo 4.bt2 is missing
+        fi
+        if [ "${BOWTIEINDEXU#*\.rev\.1\.bt2}" == "${BOWTIEINDEXU}" ]
+          then
+            >&2 echo rev.1.bt2 is missing
+        fi
+        if [ "${BOWTIEINDEXU#*\.rev\.2\.bt2}" == "${BOWTIEINDEXU}" ]
+          then
+            >&2 echo rev.2.bt2 is missing
+        fi
         >&2 echo "You are missing some index file(s), please check your input."
         debug
         exit 1;
     fi
 fi
 
-CMDLINEARGS="tophat2 ${read-mismatches} ${read-gap-length} ${read-edit-dist} ${read-realign-edit-dist} ${min-anchor} ${splice-mismatches} ${min-intron-length} ${max-intron-length} ${max-multihits} ${suppress-hits} ${max-insertion-length} ${max-deletion-length} ${quality_type} ${integer-quals} ${color} ${library-type} ${mate-inner-dist} ${mate-std-dev} ${no-novel-juncs} ${no-novel-indels} ${no-gtf-juncs} ${no-coverage-search} ${coverage-search} ${microexon-search} ${report-secondary-alignments} ${no-discordant} ${no-mixed} ${segment-mismatches} ${segment-length} ${min-coverage-intron} ${max-coverage-intron} ${min-segment-intron} ${max-segment-intron} ${keep-fasta-order} ${bowtie2-n-ceil} ${bowtie2-gbar} ${bowtie2-mp} ${bowtie2-np} ${bowtie2-rdg} ${bowtie2-rfg} ${bowtie2-score-min} ${fusion-search} ${fusion-anchor-length} ${fusion-min-dist} ${fusion-read-mismatches} ${fusion-multireads} ${fusion-multipairs} ${fusion-ignore-chromosomes} ${rg-id} ${rg-sample} ${rg-library} ${rg-description} ${rg-platform-unit} ${rg-center} ${rg-date} ${rg-platform} "
+CMDLINEARGS+="tophat2 ${read-mismatches} ${read-gap-length} ${read-edit-dist} ${read-realign-edit-dist} ${min-anchor} ${splice-mismatches} ${min-intron-length} ${max-intron-length} ${max-multihits} ${suppress-hits} ${max-insertion-length} ${max-deletion-length} ${quality_type} ${integer-quals} ${color} ${library-type} ${mate-inner-dist} ${mate-std-dev} ${no-novel-juncs} ${no-novel-indels} ${no-gtf-juncs} ${no-coverage-search} ${coverage-search} ${microexon-search} ${report-secondary-alignments} ${no-discordant} ${no-mixed} ${segment-mismatches} ${segment-length} ${min-coverage-intron} ${max-coverage-intron} ${min-segment-intron} ${max-segment-intron} ${keep-fasta-order} ${bowtie2-n-ceil} ${bowtie2-gbar} ${bowtie2-mp} ${bowtie2-np} ${bowtie2-rdg} ${bowtie2-rfg} ${bowtie2-score-min} ${fusion-search} ${fusion-anchor-length} ${fusion-min-dist} ${fusion-read-mismatches} ${fusion-multireads} ${fusion-multipairs} ${fusion-ignore-chromosomes} ${rg-id} ${rg-sample} ${rg-library} ${rg-description} ${rg-platform-unit} ${rg-center} ${rg-date} ${rg-platform} "
 
 if [ -n "${quality_file1}" ]
   then
     CMDLINEARGS+="--quals "
     #if this doesn't make sense it will fail in the next ifs
 fi
-if [ -n "${pair1}" -a -n "${pair2}" ]
-  then
+
+#the commented parts are useful if we changed this to deal with mixed cases
+
+#if [ -n "${pair1}" -a -n "${pair2}" ] #this is now useless as they are required by agave, but keep it as a flag for mixed case
+#  then
     if [ -n "${quality_file1}" -a -z "${quality_file2}" ] || [ -z "${quality_file1}" -a -n "${quality_file2}" ]
       then
         >&2 echo "You provided paired reads, but quality files only for one of the pairs. Please provide the other one as well."
         debug
         exit 1;
     fi
-fi
-if [ -z "${pair2}" -a -n "${quality_file2}" ]
-  then
-  >&2  echo "You provided quality files for pair 2, but no pair 2."
-  debug
-  exit 1;
-fi
+#fi
+#if [ -z "${pair2}" -a -n "${quality_file2}" ]
+#  then
+#  >&2  echo "You provided quality files for pair 2, but no pair 2."
+#  debug
+#  exit 1;
+#fi
 
 if [ -n "${bowtie2-preset}" ]
   then
@@ -103,18 +130,16 @@ if [ -n "${GTF}" ]
     fi
 fi
 
-CMDLINEARGS+="--num-threads 16 --output-dir output ${BASEINDEX} ${PAIR2CMD} ${PAIR2CMD} "
+CMDLINEARGS+="--num-threads 16 --output-dir output ${BASEINDEX} ${PAIR1CMD} ${PAIR2CMD} "
 
 echo ${CMDLINEARGS}
-
-exit 0 #debugging
-
 chmod +x launch.sh
 
 echo  universe                = docker >> lib/condorSubmitEdit.htc
 echo docker_image            =  cyverseuk/tophat2:v2.1.0 >> lib/condorSubmitEdit.htc
 echo executable               = ./launch.sh >> lib/condorSubmitEdit.htc
 echo transfer_input_files               = ${INPUTSU}, launch.sh >> lib/condorSubmitEdit.htc
+echo transfer_output_files              = output >> lib/condorSubmitEdit.htc
 echo arguments                          = ${CMDLINEARGS} >> lib/condorSubmitEdit.htc
 cat /mnt/data/apps/tophat2/lib/condorSubmit.htc >> lib/condorSubmitEdit.htc
 
